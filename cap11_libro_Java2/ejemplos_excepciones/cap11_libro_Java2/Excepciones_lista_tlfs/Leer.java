e4723c2394b56c0afb1be27ac45252adeec537ae
garci <garci@DESKTOP-V3B6I8E>
2022-06-01 02:43:11 +0200

    Corrección de los errores por importación

---
+++ cap11_libro_Java2/ejemplos_excepciones/cap11_libro_Java2/Excepciones_lista_tlfs/Leer.java
@@ -0,0 +1,15 @@
+package ejemplos_excepciones.cap11_libro_Java2.Excepciones_lista_tlfs;
+
+public class Leer {
+	public static void datos() {
+		
+	}
+	public static int datoInt() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public static long datoLong() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
---
+++ cap11_libro_Java2/ejemplos_excepciones/cap11_libro_Java2/Finally/Leer.java
@@ -0,0 +1,23 @@
+package ejemplos_excepciones.cap11_libro_Java2.Finally;
+
+public class Leer {
+	public static void datos() {
+		
+	}
+	public static int datoInt() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public static long datoLong() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public static String dato() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public static double datoDouble() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
